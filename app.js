71fadacd7187c0f6e62a5a8c53923777ad5dc28b
Mitchell Adair <mitchwadair@gmail.com>
2020-04-20 13:51:34 -0400

    only allow GET for users api

--- app.js
+++ app.js
@@ -14,26 +14,31 @@ const axios = require('axios');
 
 // get users API
 const getUsers = (req, res) => {
-    db.query("SELECT COUNT(*) AS users FROM channels", (err, results) => {
-        if (err) {
-            res.writeHead(500);
-            res.end(`ERROR: ${err}`);
-            return;
-        }
-        const query = url.parse(req.url, true).query;
-        res.writeHead(200);
-        if (query.json !== undefined) {
-            let responseObject = {
-                'schemaVersion': 1,
-                'label': 'users',
-                'message': results[0].users,
-                'color': 'blue'
+    if (req.method !== 'GET') {
+        res.writeHead(400);
+        res.end('Bad Request');
+    } else {
+        db.query("SELECT COUNT(*) AS users FROM channels", (err, results) => {
+            if (err) {
+                res.writeHead(500);
+                res.end(`ERROR: ${err}`);
+                return;
             }
-            res.end(JSON.stringify(responseObject));
-        } else {
-            res.end(results[0].users.toString());
-        }
-    });
+            const query = url.parse(req.url, true).query;
+            res.writeHead(200);
+            if (query.json !== undefined) {
+                let responseObject = {
+                    'schemaVersion': 1,
+                    'label': 'users',
+                    'message': results[0].users,
+                    'color': 'blue'
+                }
+                res.end(JSON.stringify(responseObject));
+            } else {
+                res.end(results[0].users.toString());
+            }
+        });
+    }
 }
 
 // API routes
