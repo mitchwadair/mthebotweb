f58aee76f73c0bdc9375dcabe49cc2d25ab382c6
Mitchell Adair <mitchwadair@gmail.com>
2020-04-23 18:11:35 -0400

    install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <v-app>
+    <v-app-bar app color="primary">
+      <div class="d-flex align-center">
+        <v-img
+          alt="MtheBot_ Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('./assets/logo.png')"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title>MtheBot_</v-toolbar-title>
+      </div>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
+
 export default {
   name: 'App',
-}
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/mthebot/' : '/',
-    chainWebpack: config => {
-        config
-        .plugin('html')
-        .tap(args => {
-          args[0].title = 'MtheBot_'
-          return args
-        })
-      }
-  }
+  "publicPath": "/",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
