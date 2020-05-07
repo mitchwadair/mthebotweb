e6083eaffeffb5a8b247ce418c84c8791d01ee3c
Mitchell Adair <mitchwadair@gmail.com>
2020-05-07 15:34:47 -0400

    start getting data from bot

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,14 +1,37 @@
 <template>
-  <v-container>
-    Content TODO
-  </v-container>
+  <div id="dashboard">
+    <v-container>
+      <v-row>
+        <v-col align="center">
+          <h1 class='display-3'>Welcome {{userData.display_name}}</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align="center">
+          {{botStatus}}
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Dashboard',
   data: function() {
-    return {};
+    return {
+      botStatus: null,
+    };
   },
+  computed: {
+    userData() {
+      return this.$store.state.userData;
+    },
+  },
+  mounted() {
+    this.axios.get('https://api.bot.mtheb.tv/users').then(res => {
+      this.botStatus = res;
+    });
+  }
 }
 </script>
