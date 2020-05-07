d911af9f25c2026e82ee5c0d2b2dfbf0a36a3a12
Mitchell Adair <mitchwadair@gmail.com>
2020-05-07 11:41:37 -0400

    update landing page for now

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -3,7 +3,7 @@
         <h1 class='header display-4'>MtheBot_</h1>
         <span class='headline'>An easy-to-use chatbot for your Twitch channel</span>
         <div class='feature-container'>
-            <v-carousel cycle show-arrows-on-hover hide-delimiter-background height="20rem">
+            <!--v-carousel cycle show-arrows-on-hover hide-delimiter-background height="20rem">
                 <v-carousel-item v-for="(slide, i) in slides" :key="i">
                     <v-sheet height="100%" tile>
                         <v-row class='fill-height' align="center">
@@ -16,11 +16,24 @@
                         </v-row>
                     </v-sheet>
                 </v-carousel-item>
-            </v-carousel>
+            </v-carousel-->
         </div>
         <v-container>
             <v-row align="center">
-                <v-spacer></v-spacer>
+                <v-col align="center">
+                    <v-card flat>
+                        <v-card-title class='justify-center'>Contact</v-card-title>
+                        <v-card-text>Open an issue, or send an email with questions or concerns</v-card-text>
+                        <v-card-text class='pt-0'>
+                            <v-btn outlined href="mailto:mthebot6969@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <v-icon left>mdi-email</v-icon>Email
+                            </v-btn>
+                            <v-btn style="margin-left: .5rem" outlined href="https://github.com/mitchwadair/mthebot/issues" target="_blank" rel="noopener noreferrer">
+                                <v-icon left>mdi-github</v-icon>Open an Issue
+                            </v-btn>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
                 <v-col align="center">
                     <v-card flat>
                         <v-card-title class='justify-center'>GetStarted</v-card-title>
@@ -46,7 +59,6 @@
                         </v-card-text>
                     </v-card>
                 </v-col>
-                <v-spacer></v-spacer>
             </v-row>
         </v-container>
     </div>
@@ -79,9 +91,10 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
 }
 .header {
-    padding: 3rem 0 2rem 0;
+    padding: 2rem 0 2rem 0;
 }
 .feature-container {
     width: 800px;
