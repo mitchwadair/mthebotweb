239dcb8f515e9c87dc6205aa879524654740b540
Mitchell Adair <mitchwadair@gmail.com>
2020-05-09 16:58:59 -0400

    build initial dashboard page

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,26 +7,70 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col align="center">
-          <div v-if="botStatus">
-            MtheBot_ is currently enabled in your chat!
-            <v-btn @click="disableBot" depressed color="primary">Disable MtheBot_</v-btn>
-          </div>
-          <div v-else>
-            MtheBot_ is currently disabled in your chat!
-          </div>
+        <v-col>
+          <v-card v-if="botStatus" flat style="margin: 0 6rem">
+            <v-card-title>MtheBot_ is Enabled</v-card-title>
+            <v-card-text>MtheBot_ is currently sitting in your chat awaiting commands! You may disable MtheBot_ and it will leave your chat.</v-card-text>
+            <v-card-actions>
+                <v-btn @click="disableBot" text color="primary">Disable MtheBot_</v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card v-else flat style="margin: 0 6rem">
+            <v-card-title>MtheBot_ is Disabled</v-card-title>
+            <v-card-text>MtheBot_ must be enabled in order to enhance your chat experience.  Enable MtheBot_ so it can join your chat and make sure to give it moderator status to avoid any issues.</v-card-text>
+            <v-card-actions>
+                <v-btn @click="enableBot" text color="primary">Enable MtheBot_</v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
+      </v-row>
+      <v-row v-if="botStatus" class='mt-5'>
+        <v-spacer></v-spacer>
+        <v-col v-for="(cardData, i) in navCardData" :key="i">
+          <v-card height="100%" class='d-flex flex-column'>
+            <v-card-title>{{cardData.title}}</v-card-title>
+            <v-card-text>{{cardData.content}}</v-card-text>
+            <v-card-actions class='mt-auto'>
+                <v-btn text color="primary" :to="cardData.route">{{cardData.action}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+      <v-row class='mt-5'>
+        <v-spacer></v-spacer>
+        <v-col v-for="(cardData, i) in infoCardData" :key="i">
+          <v-card height="100%" class='d-flex flex-column'>
+            <v-card-title>{{cardData.title}}</v-card-title>
+            <v-card-text>{{cardData.content}}</v-card-text>
+            <v-card-actions class='mt-auto'>
+                <v-btn text color="primary" :to="cardData.route">{{cardData.action}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-spacer></v-spacer>
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import defaultEvents from '../defaults/events.json';
+
 export default {
   name: 'Dashboard',
   data: function() {
     return {
       botStatus: null,
+      navCardData: [
+        {title: 'Commands', content: 'Modify MtheBot_\'s commands for your channel', route:'/commands', action: 'Go to Commands'},
+        {title: 'Events', content: 'Modify MtheBot_\'s channel event messages', route:'/events', action: 'Go to Events'},
+        {title: 'Timed Messages', content: 'Modify MtheBot_\'s timed channel messages', route:'/timers', action: 'Go to Timers'},
+      ],
+      infoCardData: [
+        {title: 'About', content: 'Learn more about MtheBot_', route:'/about', action: 'About'},
+        {title: 'Support', content: 'Consider supporting the development and maintenance of MtheBot_ by supporting me on Patreon!', route:'#', action: 'MtheBot_ on Patreon'},
+      ]
     };
   },
   computed: {
@@ -45,7 +89,23 @@ export default {
       const channel = this.$store.state.userData.login;
       this.axios.delete(`https://api.bot.mtheb.tv/chats/${channel}`).then(() => {
         this.botStatus = false;
-      })
+      });
+    },
+    enableBot: function() {
+      const channel = this.$store.state.userData.login;
+      this.axios.get(`https://api.bot.mtheb.tv/chats/${channel}`).then(res => {
+        let promises = []
+        if (res.status === 404) {
+          promises.push(this.axios.post(`https://api.bot.mtheb.tv/commands/${channel}`, {}));
+          promises.push(this.axios.post(`https://api.bot.mtheb.tv/timers/${channel}`, {}));
+          promises.push(this.axios.post(`https://api.bot.mtheb.tv/commands/${channel}`, defaultEvents));
+        }
+        Promise.all(promises).then(() => {
+          this.axios.post(`https://api.bot.mtheb.tv/chats/${channel}`).then(() => {
+            this.botStatus = true;
+          });
+        });
+      });
     }
   }
 }
