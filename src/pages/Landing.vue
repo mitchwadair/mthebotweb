23d9355b526031cdd3d30f75d5dd5d8ba0bd97bc
Mitchell Adair <mitchwadair@gmail.com>
2020-05-11 15:07:04 -0400

    improve actions on landing cards

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -31,22 +31,26 @@
                             MtheBot_ is an open-source project by 
                             <a href="https://github.com/mitchwadair" target="_blank" rel="noopener noreferrer">Mitchell Adair</a>
                         </v-card-text>
-                        <v-card-text class='pt-0 mt-auto'>
+                        <v-card-actions class='pt-0 mt-auto'>
+                            <v-spacer/>
                             <v-btn outlined href="https://github.com/mitchwadair/mthebot" target="_blank" rel="noopener noreferrer">
                                 <v-icon left>mdi-github</v-icon>MtheBot_ on GitHub
                             </v-btn>
-                        </v-card-text>
+                            <v-spacer/>
+                        </v-card-actions>
                     </v-card>
                 </v-col>
                 <v-col align="center" justify="center">
                     <v-card flat height="100%" class='d-flex flex-column'>
                         <v-card-title class='justify-center'>Get Started</v-card-title>
                         <v-card-text>Login with Twitch to start improving your chat's experience!</v-card-text>
-                        <v-card-text class='pt-0 mt-auto'>
+                        <v-card-actions class='pt-0 mt-auto'>
+                            <v-spacer/>
                             <v-btn @click="login" depressed color="#6441A5" style="color: white">
                                 <v-icon left>mdi-twitch</v-icon>Login
                             </v-btn>
-                        </v-card-text>
+                            <v-spacer/>
+                        </v-card-actions>
                     </v-card>
                 </v-col>
                 <v-col align="center" justify="center">
@@ -55,11 +59,13 @@
                         <v-card-text>
                             MtheBot_ is currently being developed by a one-person team, and being paid for out-of-pocket.  Any help supporting the project is appreciated!
                         </v-card-text>
-                        <v-card-text class='pt-0 mt-auto'>
+                        <v-card-actions class='pt-0 mt-auto'>
+                            <v-spacer/>
                             <v-btn outlined href="https://www.patreon.com/mitchdev" target="_blank" rel="noopener noreferrer">
                                 <v-icon left>mdi-patreon</v-icon>MtheBot_ on Patreon
                             </v-btn>
-                        </v-card-text>
+                            <v-spacer/>
+                        </v-card-actions>
                     </v-card>
                 </v-col>
                 <v-spacer/>
