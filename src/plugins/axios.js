061ea4af429efe7a871d2fbd9c10590bdc0a8d18
Mitchell Adair <mitchwadair@gmail.com>
2020-05-15 12:50:59 -0400

    fix local dev config

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let instance = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : "https://api.bot.mtheb.tv",
+    baseURL: process.env.NODE_ENV == 'development' ? process.env.VUE_APP_BASE_URL : "https://api.bot.mtheb.tv",
 });
 
 export default {
