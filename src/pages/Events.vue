1e4a611f70cb1a2d8d320eceb75a8363a0b984e8
Mitchell Adair <mitchwadair@gmail.com>
2021-01-18 15:17:37 -0500

    overhaul events page for UX/size

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,5 +1,63 @@
 <template>
     <div id="events">
+        <v-dialog v-model="modifyDialog" attach="#events" persistent max-width="50rem">
+            <v-card>
+                <v-card-title>Modify {{eventLabels[newEventData.name]}}</v-card-title>
+                <v-card-subtitle>Update the message displayed when a {{eventLabels[newEventData.name]}} happens.</v-card-subtitle>
+                <v-card-text align="right">
+                    <v-menu offset-y left close-on-click open-on-hover>
+                        <template v-slot:activator="{on}">
+                            <v-btn text outlined v-on="on" :ripple=false>
+                                Insert Data Tag
+                            </v-btn>
+                        </template>
+
+                        <v-list dense>
+                            <v-list-item v-for='tag in dataTags.general' :key='tag.label' @click="insertDataTag(tag)">
+                                <v-tooltip left nudge-left=10>
+                                    <template v-slot:activator="{on}">
+                                        <v-list-item-content v-on="on">
+                                            <v-list-item-title>{{tag.label}}</v-list-item-title>
+                                        </v-list-item-content>
+                                    </template>
+                                    <span>{{tag.info}}</span>
+                                </v-tooltip>
+                            </v-list-item>
+                            <v-list-item v-for='tag in dataTags[newEventData.name]' :key='tag.label' @click="insertDataTag(tag)">
+                                <v-tooltip left nudge-left=10>
+                                    <template v-slot:activator="{on}">
+                                        <v-list-item-content v-on="on">
+                                            <v-list-item-title>{{tag.label}}</v-list-item-title>
+                                        </v-list-item-content>
+                                    </template>
+                                    <span>{{tag.info}}</span>
+                                </v-tooltip>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </v-card-text>
+                <v-card-text>
+                    <v-form v-model="modifyValid">
+                        <v-textarea
+                            v-model="newEventData.message"
+                            hide-details="auto"
+                            label="Message"
+                            maxlength="500"
+                            :ref="'modify-textarea'"
+                            :rules="[validationRules.required]"
+                            outlined dense counter auto-grow required/>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer/>
+                    <v-btn color="primary" text @click="cancelModify()">Cancel</v-btn>
+                    <v-btn color="primary" text @click="{if (modifyValid) {updateData()}}">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+            <v-overlay :value="isSending" opacity=".15" absolute>
+                <v-progress-circular indeterminate color="primary" size="64"/>
+            </v-overlay>
+        </v-dialog>
         <v-container>
             <v-row>
                 <v-col class='mx-3'>
@@ -13,107 +71,30 @@
                 </v-col>
             </v-row>
             <v-row v-else>
-                <v-col class='px-0 px-md-3'>
-                    <v-sheet tile elevation="4" class='mx-4'>
-                        <v-list class='pt-0'>
-                            <template v-for="(event, i) in channelData">
-                                <v-list-item :key="'event' + i">
-                                    <v-list-item-content>
-                                        <v-list-item-title class='font-weight-medium'>
-                                            {{eventLabels[event.name]}}
-                                            <v-chip v-if="event.enabled" color="success" x-small label class='ml-2 px-2'>Enabled</v-chip>
-                                            <v-chip v-else color="error" x-small label class='ml-2 px-2'>Disabled</v-chip>
-                                        </v-list-item-title>
-                                        <v-row class='mb-n4 mt-n2 flex-nowrap'>
-                                            <v-col>
-                                                <v-list-item-subtitle>Message</v-list-item-subtitle>
-                                                "{{event.message}}"
-                                            </v-col>
-                                            <v-col class='flex-grow-0'>
-                                                <v-menu offset-y left>
-                                                    <template v-slot:activator="{on}">
-                                                        <v-btn icon v-on="on" :ripple=false>
-                                                            <v-icon>mdi-dots-vertical</v-icon>
-                                                        </v-btn>
-                                                    </template>
-                                                    <v-list dense>
-                                                        <v-list-item @click="cacheCurrentData(event); $set(modifyDialog, event.name, true);">
-                                                            <v-list-item-content>
-                                                                <v-list-item-title>Modify</v-list-item-title>
-                                                            </v-list-item-content>
-                                                        </v-list-item>
-                                                        <v-list-item @click="flipEventStatus(event)">
-                                                            <v-list-item-content>
-                                                                <v-list-item-title>{{event.enabled ? 'Disable' : 'Enable'}}</v-list-item-title>
-                                                            </v-list-item-content>
-                                                        </v-list-item>
-                                                    </v-list>
-                                                </v-menu>
-                                                <v-dialog v-model="modifyDialog[event.name]" attach="#events" persistent max-width="50rem">
-                                                    <v-card>
-                                                        <v-card-title>Modify {{eventLabels[event.name]}}</v-card-title>
-                                                        <v-card-subtitle>Update the message displayed when a {{eventLabels[event.name]}} happens.</v-card-subtitle>
-                                                        <v-card-text align="right">
-                                                            <v-menu offset-y left close-on-click open-on-hover>
-                                                                <template v-slot:activator="{on}">
-                                                                    <v-btn text outlined v-on="on" :ripple=false>
-                                                                        Insert Data Tag
-                                                                    </v-btn>
-                                                                </template>
-
-                                                                <v-list dense>
-                                                                    <v-list-item v-for='tag in dataTags.general' :key='tag.label' @click="insertDataTag(tag, i)">
-                                                                        <v-tooltip left nudge-left=10>
-                                                                            <template v-slot:activator="{on}">
-                                                                                <v-list-item-content v-on="on">
-                                                                                    <v-list-item-title>{{tag.label}}</v-list-item-title>
-                                                                                </v-list-item-content>
-                                                                            </template>
-                                                                            <span>{{tag.info}}</span>
-                                                                        </v-tooltip>
-                                                                    </v-list-item>
-                                                                    <v-list-item v-for='tag in dataTags[event.name]' :key='tag.label' @click="insertDataTag(tag, i)">
-                                                                        <v-tooltip left nudge-left=10>
-                                                                            <template v-slot:activator="{on}">
-                                                                                <v-list-item-content v-on="on">
-                                                                                    <v-list-item-title>{{tag.label}}</v-list-item-title>
-                                                                                </v-list-item-content>
-                                                                            </template>
-                                                                            <span>{{tag.info}}</span>
-                                                                        </v-tooltip>
-                                                                    </v-list-item>
-                                                                </v-list>
-                                                            </v-menu>
-                                                        </v-card-text>
-                                                        <v-card-text>
-                                                            <v-form v-model="modifyValid[i]">
-                                                                <v-textarea
-                                                                    v-model="event.message"
-                                                                    hide-details="auto"
-                                                                    label="Message"
-                                                                    maxlength="500"
-                                                                    :ref="'textarea' + i"
-                                                                    :rules="[validationRules.required]"
-                                                                    outlined dense counter auto-grow required/>
-                                                            </v-form>
-                                                        </v-card-text>
-                                                        <v-card-actions>
-                                                            <v-spacer/>
-                                                            <v-btn color="primary" text @click="$set(modifyDialog, event.name, false); cancelModify()">Cancel</v-btn>
-                                                            <v-btn color="primary" text @click="if (modifyValid[i]) {updateData(event)}">Save</v-btn>
-                                                        </v-card-actions>
-                                                    </v-card>
-                                                    <v-overlay :value="isSending" opacity=".15" absolute>
-                                                        <v-progress-circular indeterminate color="primary" size="64"/>
-                                                    </v-overlay>
-                                                </v-dialog>
-                                            </v-col>
-                                        </v-row>
-                                    </v-list-item-content>
-                                </v-list-item>
-                                <v-divider v-if="i < Object.keys(channelData).length - 1" :key="i" class='mx-4'/>
-                            </template>
-                        </v-list>
+                <v-col class='mx-2'>
+                    <v-sheet tile elevation="0" class='mx-auto flex-grow-1' style='background-color: rgba(0, 0, 0, 0); max-width: 600px'>
+                        <template v-for="(event, i) in channelData">
+                            <v-card :key="'event' + i" class='mx-auto mt-2' min-width="90%" max-width="90%" elevation="4" color="secondary">
+                                <v-card-title class='pb-2'>
+                                    {{eventLabels[event.name]}}
+                                    <v-chip v-if="event.enabled" color="success" x-small label class='ml-2 px-2'>Enabled</v-chip>
+                                    <v-chip v-else color="error" x-small label class='ml-2 px-2'>Disabled</v-chip>
+                                </v-card-title>
+                                <v-card-text class='pb-0 pt-2'>
+                                    <v-row>
+                                        <v-col class='pa-0'>
+                                            <v-card-subtitle class='py-0 subtitle-2'>Message</v-card-subtitle>
+                                            <v-card-text class='text--primary'>"{{event.message}}"</v-card-text>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-text>
+                                <v-card-actions class='pt-0'>
+                                    <v-btn text @click="modifyEvent(i)" color="primary">Modify</v-btn>
+                                    <v-btn text @click="flipEventStatus(event)" color="primary">{{event.enabled ? 'Disable' : 'Enable'}}</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                            <v-divider v-if="i < Object.keys(channelData).length - 1" :key="i" class='mx-4'/>
+                        </template>
                     </v-sheet>
                 </v-col>
             </v-row>
@@ -177,9 +158,10 @@ export default {
                 cheer: 'Cheer'
             },
             channelData: {},
-            dataCache: {},
-            modifyDialog: {},
-            modifyValid: {},
+            newEventData: {},
+            modifyDialog: false,
+            modifyValid: true,
+            toModify: null,
             validationRules: validationRules,
             loadingData: true,
             isSending: false,
@@ -199,40 +181,41 @@ export default {
         },
         flipEventStatus: function(event) {
             const channel = this.$store.state.userData.id;
-            event.enabled = !event.enabled;
-            this.axios.put(`/events/${channel}/${event.name}`, {message: event.message, enabled: event.enabled}, {headers:{'Authorization': `Bearer ${this.$auth.accessToken}`}}).then(() => {
+            this.axios.put(`/events/${channel}/${event.name}`, {message: event.message, enabled: !event.enabled}, {headers:{'Authorization': `Bearer ${this.$auth.accessToken}`}}).then(() => {
                 this.isSending = false;
+                event.enabled = !event.enabled;
             }).catch(() => {
                 this.isSending = false;
                 event.enabled = !event.enabled;
             });
         },
-        cacheCurrentData: function(event) {
-            this.responseError = null;
-            this.dataCache = JSON.parse(JSON.stringify(event));
+        modifyEvent: function(index) {
+            this.modifyDialog = true;
+            this.newEventData = {...this.channelData[index]};
+            this.toModify = index;
         },
-        updateData: function(eventData) {
+        updateData: function() {
             const channel = this.$store.state.userData.id;
             this.isSending = true;
-            this.axios.put(`/events/${channel}/${this.dataCache.name}`, {message: eventData.message, enabled: eventData.enabled}, {headers:{'Authorization': `Bearer ${this.$auth.accessToken}`}}).then(() => {
+            this.axios.put(`/events/${channel}/${this.newEventData.name}`, {message: this.newEventData.message, enabled: this.newEventData.enabled}, {headers:{'Authorization': `Bearer ${this.$auth.accessToken}`}}).then(() => {
                 this.isSending = false;
-                this.modifyDialog[eventData.name] = false;
+                this.modifyDialog = false;
+                this.channelData[this.toModify] = {...this.newEventData};
+                this.newEventData = {};
             }).catch(err => {
                 this.isSending = false;
                 this.responseError = err.response.data;
             });
         },
         cancelModify: function() {
-            let undo = this.channelData.find(e => e.name === this.dataCache.name);
-            undo.name = this.dataCache.name;
-            undo.message = this.dataCache.message;
-            undo.enabled = this.dataCache.enabled;
+            this.modifyDialog = false;
+            this.newEventData = {};
         },
-        insertDataTag: function(tag, index) {
-            const el = this.$refs[`textarea${index}`][0].$el.querySelector('textarea');
+        insertDataTag: function(tag) {
+            const el = this.$refs['modify-textarea'].$el.querySelector('textarea');
 
             let cursorPos = el.selectionEnd;
-            this.channelData[index].message = `${this.channelData[index].message.substring(0, cursorPos)}${tag.tag}${this.channelData[index].message.substring(cursorPos)}`;
+            this.newEventData.message = `${this.newEventData.message.substring(0, cursorPos)}${tag.tag}${this.newEventData.message.substring(cursorPos)}`;
 
             cursorPos += tag.tag.length;
             this.$nextTick(() => {
